50a44784a4b5945f20b6b5a032cbdc44094d4e26
chriswz1998 <scissorwz@gmail.com>
2023-11-16 22:37:28 -0800

    fix a little bit display problem

--- app/(marketing)/_components/Container5.tsx
+++ app/(marketing)/_components/Container5.tsx
@@ -12,7 +12,7 @@ const Container5 = () => {
             <div className={'rounded-lg overflow-hidden bg-mask-color1 w-[460px] h-[360px] flex justify-center items-center'}>
               <TrendingUp size={120}  className={'text-default-blue'}/>
             </div>
-            <p className={'pt-8 pb-5 text-description-text-color1 text-[20px]'}>客户同比优势不足，我们将通过率提升至100%</p>
+            <p className={'max-w-[460px] pt-8 pb-5 text-description-text-color1 text-[20px]'}>客户同比优势不足，我们将通过率提升至100%</p>
             <p className={'text-default-blue font-bold text-[36px]'}>100%与客户共同成长</p>
           </div>
 
@@ -20,7 +20,7 @@ const Container5 = () => {
             <div className={'shadow-[0_6.42px_33px_rgba(61,53,255,0.4)] rounded-lg overflow-hidden bg-default-blue w-[460px] h-[440px] flex justify-center items-center'}>
               <TrendingUp size={120}  className={'text-white'}/>
             </div>
-            <p className={'pt-8 pb-5 text-description-text-color1 text-[20px]'}>同比去年雇主担保通过率在三个月我们努力将客户服务做到最好，实内提升至3倍?</p>
+            <p className={'max-w-[460px] pt-8 pb-5 text-description-text-color1 text-[20px]'}>同比去年雇主担保通过率在三个月我们努力将客户服务做到最好，实内提升至3倍?</p>
             <p className={'text-default-blue font-bold text-[36px]'}>3X 我们的独门内功!</p>
           </div>
 
@@ -28,7 +28,7 @@ const Container5 = () => {
             <div className={'rounded-lg overflow-hidden bg-mask-color1 w-[460px] h-[360px] flex justify-center items-center'}>
               <TrendingUp size={120}  className={'text-default-blue'}/>
             </div>
-            <p className={'pt-8 pb-5 text-description-text-color1 text-[20px]'}>我们努力将客户服务做到最好，实现O差评!</p>
+            <p className={'max-w-[460px] pt-8 pb-5 text-description-text-color1 text-[20px]'}>我们努力将客户服务做到最好，实现O差评!</p>
             <p className={'text-default-blue font-bold text-[36px]'}>客户优先,服务第一</p>
           </div>
         </div>
