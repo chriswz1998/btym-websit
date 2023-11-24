7c6ada539cc4716b69e218c504f923e02ee59eb7
chriswz1998 <scissorwz@gmail.com>
2023-11-23 22:17:29 -0800

    fix some problem, running in the ec2 servers

--- pgsql/index.ts
+++ pgsql/index.ts
@@ -4,15 +4,13 @@ const prisma = new PrismaClient()
 
 async function main() {
   // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  // const allUsers = await prisma.user.findMany()
+  // console.log(allUsers)
 }
 
-main()
-.then(async () => {
+main().then(async () => {
   await prisma.$disconnect()
-})
-.catch(async (e) => {
+}).catch(async (e) => {
   console.error(e)
   await prisma.$disconnect()
   process.exit(1)
