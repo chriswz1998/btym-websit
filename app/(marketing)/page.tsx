a42f74af7100f55c95d12898dac7e6c211bf7973
chriswz1998 <scissorwz@gmail.com>
2024-01-31 22:53:18 -0800

    first page ui all fixed.

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,16 +1,12 @@
 'use client'
 
-import { ArrowLeft } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import { Navbar } from '@/components/navbar'
-import { Province_banner } from '@/components/province_banner'
 import { Hot_project_item } from '@/app/(marketing)/_components/hot_project_item'
 import { how_item_res } from '@/lib/publicResuces'
 import Link from 'next/link'
 import { DialogCoverFrom } from '@/components/dialog'
-import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
-import Autoplay from 'embla-carousel-autoplay'
 import React from 'react'
 import {Carousels} from "@/app/(marketing)/_components/carousel";
 
@@ -19,163 +15,161 @@ export default function MarketingPage(){
     return (<div>
         <Navbar/>
         <Carousels/>
+
         <div className={'flex items-center flex-wrap py-5'}>
-            <div className='flex items-center bg-green-200 p-5 md:mx-auto'>
-                <ArrowLeft/>
-                移民行业旗舰 品牌信誉保证
+            <div className='flex items-center bg-mask-color/10 rounded-lg p-5 md:mx-auto space-x-4'>
+                <Image src={'/icon131-1.svg'} alt={''} height={30} width={30}/>
+                <span className={'font-bold text-blue-900'}>移民行业旗舰 品牌信誉保证</span>
             </div>
-            <div className='flex items-center bg-green-200 p-5 md:mx-auto'>
-                <ArrowLeft/>
-                移民行业旗舰 品牌信誉保证
+            <div className='flex items-center bg-mask-color/10 rounded-lg p-5 md:mx-auto space-x-4'>
+                <Image src={'/icon131.svg'} alt={''} height={30} width={30}/>
+                <span className={'font-bold text-blue-900'}>加国精英团队 提供专业服务</span>
             </div>
-            <div className='flex items-center bg-green-200 p-5 md:mx-auto'>
-                <ArrowLeft/>
-                移民行业旗舰 品牌信誉保证
+            <div className='flex items-center bg-mask-color/10 rounded-lg p-5 md:mx-auto space-x-4'>
+                <Image src={'/icon131-2.svg'} alt={''} height={30} width={30}/>
+                <span className={'font-bold text-blue-900'}>CICC持牌移民顾问亲审文案</span>
             </div>
-            <div className='flex items-center bg-green-200 p-5 md:mx-auto'>
-                <ArrowLeft/>
-                移民行业旗舰 品牌信誉保证
+            <div className='flex items-center bg-mask-color/10 rounded-lg p-5 md:mx-auto space-x-4'>
+                <Image src={'/icon131-3.svg'} alt={''} height={30} width={30}/>
+                <span className={'font-bold text-blue-900'}>全程一站式无忧服务</span>
             </div>
         </div>
 
-        {/*每年有2000+选择移民加拿大*/}
-        <div className={ 'py-16  flex flex-wrap justify-center' }>
-            { how_item_res.map((item, index) => {
-                return (<Hot_project_item url={ item.url } href={ item.href } text={ item.text } key={ index }/>)
-            }) }
+        <div className={'py-16  flex flex-wrap justify-center'}>
+            {how_item_res.map((item, index) => {
+                return (<Hot_project_item url={item.url} href={item.href} text={item.text} key={index}/>)
+            })}
         </div>
 
-        <div className={ 'space-x-20 text-center' }>
-            <DialogCoverFrom text={ '资深顾问全面解答' } variant={ 'ghost' }
-                             className={ 'bg-[#11437F] hover:bg-[#11437F] w-[300px] h-[66px] text-white hover:text-white px-4 py-2.5 rounded-[10px] shadow' }/>
-            <Link href={ '/employer' }>
+        <div className={'space-x-20 text-center'}>
+            <DialogCoverFrom text={'资深顾问全面解答'} variant={'ghost'}
+                             className={'bg-[#11437F] hover:bg-[#11437F] w-[300px] h-[66px] text-white hover:text-white px-4 py-2.5 rounded-[10px] shadow'}/>
+            <Link href={'/employer'}>
                 <Button
-                    className={ 'bg-[#E83328] hover:bg-[#E83328] w-[300px] h-[66px] px-4 py-2.5 rounded-[10px] shadow' }>了解更多福利</Button>
+                    className={'bg-[#E83328] hover:bg-[#E83328] w-[300px] h-[66px] px-4 py-2.5 rounded-[10px] shadow'}>了解更多福利</Button>
             </Link>
 
         </div>
 
-        {/*博泰专注加拿大移民*/ }
-        <div
-            className={ 'p-20 pt-30 text-center text-black text-[50px] font-semibold font-[\'Maven Pro\'] space-y-4' }>
+        <div className={'p-20 pt-30 text-center text-black text-[50px] font-semibold font-[\'Maven Pro\'] space-y-4'}>
             <div>博泰专注加拿大移民</div>
             <div
                 className="text-stone-500 text-[40px] font-normal font-['Noto Sans HK'] leading-10">
                 致力于为中国申请人带来优质的加拿大移民服务
             </div>
         </div>
-        <div className={ 'relative w-full h-[460px]' }>
-            <div className={ 'absolute top-0 left-0 w-full h-full' }>
-                <Province_banner banner={ '/pki4492111.png' }/>
+
+        <div className={'relative w-full h-[460px]'}>
+            <div className={'absolute top-0 left-0 w-full h-full'}>
+                <Image src={'/pki4492111.png'} alt={'/'} layout='fill'/>
             </div>
             <div
-                className={ 'px-20 flex items-center justify-around text-white w-full h-full absolute left-0 top-0 bg-gradient-to-r from-[#062F7E] to-[#3B63B173]' }>
-                <div className={ 'space-y-4 text-center' }>
+                className={'px-20 flex items-center justify-around text-white w-full h-full absolute left-0 top-0 bg-gradient-to-r from-[#062F7E] to-[#3B63B173]'}>
+                <div className={'space-y-4 text-center'}>
                     <span className="text-6xl font-bold">3</span>
                     <span className="text-3xl">家+</span>
-                    <p className={ 'text-xl' }>加拿大直营公司,
+                    <p className={'text-xl'}>加拿大直营公司,
                         <br/>
-                   加拿大东部，中部，西部项目全覆盖</p>
+                        加拿大东部，中部，西部项目全覆盖</p>
                 </div>
-                <div className={ 'w-[1px] h-[110px] bg-white' }></div>
-                <div className={ 'space-y-4 text-center' }>
+                <div className={'w-[1px] h-[110px] bg-white'}></div>
+                <div className={'space-y-4 text-center'}>
                     <span className="text-6xl font-bold">10</span>
                     <span className="text-3xl">年+</span>
-                    <p className={ 'text-xl' }>加拿大本土移民服务经验</p>
+                    <p className={'text-xl'}>加拿大本土移民服务经验</p>
                 </div>
-                <div className={ 'w-[1px] h-[110px] bg-white' }></div>
-                <div className={ 'space-y-4 text-center' }>
+                <div className={'w-[1px] h-[110px] bg-white'}></div>
+                <div className={'space-y-4 text-center'}>
                     <span className="text-6xl font-bold">4</span>
                     <span className="text-3xl">位+</span>
-                    <p className={ 'text-xl' }>加拿大持牌移民顾问</p>
+                    <p className={'text-xl'}>加拿大持牌移民顾问</p>
                 </div>
-                <div className={ 'w-[1px] h-[110px] bg-white' }></div>
-                <div className={ 'space-y-4 text-center' }>
+                <div className={'w-[1px] h-[110px] bg-white'}></div>
+                <div className={'space-y-4 text-center'}>
                     <span className="text-6xl font-bold">200</span>
                     <span className="text-3xl">+</span>
-                    <p className={ 'text-xl' }>大量真实成功案例</p>
+                    <p className={'text-xl'}>大量真实成功案例</p>
                 </div>
             </div>
-        </div>
-        <Province_banner banner={ '/581703237163_.jpg' }/>
-        <div className={ 'bg-[#EBF3FC] py-20 space-x-20 text-center' }>
-            <Button
-                className={ 'bg-[#11437F] hover:bg-[#11437F] w-[300px] h-[66px] px-4 py-2.5 rounded-[10px] shadow' }>移民专家在线解答</Button>
-            <DialogCoverFrom text={ '预约顾问评估' } variant={ 'ghost' }
-                             className={ 'bg-[#E83328] hover:bg-[#E83328] w-[300px] h-[66px] text-white hover:text-white px-4 py-2.5 rounded-[10px] shadow' }/>
+            <Image src={'/581703237163_.jpg'} alt={'/'} layout='fill'/>
         </div>
 
-        {/*博泰8大服务保障*/ }
-        <div className={ 'relative  w-full h-[863px]' }>
-            <div className={ 'absolute top-0 left-0 w-full h-full' }>
-                <Province_banner banner={ '/bg8dfw.png' }/>
+        <div className={'bg-[#EBF3FC] py-20 space-x-20 text-center'}>
+            <Button
+                className={'bg-[#11437F] hover:bg-[#11437F] w-[300px] h-[66px] px-4 py-2.5 rounded-[10px] shadow'}>移民专家在线解答</Button>
+            <DialogCoverFrom text={'预约顾问评估'} variant={'ghost'}
+                             className={'bg-[#E83328] hover:bg-[#E83328] w-[300px] h-[66px] text-white hover:text-white px-4 py-2.5 rounded-[10px] shadow'}/>
+        </div>
+
+        <div className={'relative  w-full h-[863px]'}>
+            <div className={'absolute top-0 left-0 w-full h-full'}>
+                <Image src={'/bg8dfw.png'} alt={'/'} layout='fill'/>
             </div>
             <div
-                className={ 'absolute top-0 left-0 w-full h-full bg-v1-bg1 flex flex-col justify-center space-y-20' }>
-                <div className={ 'text-center text-white text-[40px] font-bold font-[\'Maven Pro\']' }>
+                className={'absolute top-0 left-0 w-full h-full bg-v1-bg1 flex flex-col justify-center space-y-20'}>
+                <div className={'text-center text-white text-[40px] font-bold font-[\'Maven Pro\']'}>
                     博泰<span className="text-red-600">8</span>大服务保障
-                    <p className={ 'text-[30px] font-light' }>加拿大移民福利——不可拒绝的加拿大移民理由</p>
+                    <p className={'text-[30px] font-light'}>加拿大移民福利——不可拒绝的加拿大移民理由</p>
                 </div>
-                <div className={ 'flex flex-wrap justify-center' }>
-                    <div
-                        className={ 'm-4 w-[300px] h-[190px] bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4' }>
-                        <Image src={ '/Group4591.svg' } alt={ '' } width={ 70 } height={ 70 }/>
+                <div className={'grid grid-cols-4'}>
+                    <div className={'py-5 m-5 bg-white rounded-lg shadow-lg space-y-4 flex flex-col justify-center items-center'}>
+                        <Image src={'/Group4591.svg'} alt={''} width={70} height={70}/>
                         <p
                             className="text-black text-2xl font-bold">
                             量身定制方案
                         </p>
                     </div>
                     <div
-                        className={ 'm-4 w-[300px] h-[190px] bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4' }>
-                        <Image src={ '/Group4592.svg' } alt={ '' } width={ 70 } height={ 70 }/>
+                        className={'py-5 m-5 bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4'}>
+                        <Image src={'/Group4592.svg'} alt={''} width={70} height={70}/>
                         <p
                             className="text-black text-2xl font-bold">
                             中加两地直营
                         </p>
                     </div>
                     <div
-                        className={ 'm-4 w-[300px] h-[190px] bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4' }>
-                        <Image src={ '/Group4593.svg' } alt={ '' } width={ 70 } height={ 70 }/>
+                        className={'py-5 m-5 bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4'}>
+                        <Image src={'/Group4593.svg'} alt={''} width={70} height={70}/>
                         <p
                             className="text-black text-2xl font-bold">
                             本土团队
                         </p>
                     </div>
                     <div
-                        className={ 'm-4 w-[300px] h-[190px] bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4' }>
-                        <Image src={ '/Group4594.svg' } alt={ '' } width={ 70 } height={ 70 }/>
+                        className={'py-5 m-5 bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4'}>
+                        <Image src={'/Group4594.svg'} alt={''} width={70} height={70}/>
                         <p
                             className="text-black text-2xl font-bold">
                             多重材料审核
                         </p>
                     </div>
                     <div
-                        className={ 'm-4 w-[300px] h-[190px] bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4' }>
-                        <Image src={ '/Group4595.svg' } alt={ '' } width={ 70 } height={ 70 }/>
+                        className={'py-5 m-5 bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4'}>
+                        <Image src={'/Group4595.svg'} alt={''} width={70} height={70}/>
                         <p
                             className="text-black text-2xl font-bold">
                             成功后再收费
                         </p>
                     </div>
                     <div
-                        className={ 'm-4 w-[300px] h-[190px] bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4' }>
-                        <Image src={ '/Group4596.svg' } alt={ '' } width={ 70 } height={ 70 }/>
+                        className={'py-5 m-5 bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4'}>
+                        <Image src={'/Group4596.svg'} alt={''} width={70} height={70}/>
                         <p
                             className="text-black text-2xl font-bold">
                             协助拒签复议
                         </p>
                     </div>
                     <div
-                        className={ 'm-4 w-[300px] h-[190px] bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4' }>
-                        <Image src={ '/Group4597.svg' } alt={ '' } width={ 70 } height={ 70 }/>
+                        className={'py-5 m-5 bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4'}>
+                        <Image src={'/Group4597.svg'} alt={''} width={70} height={70}/>
                         <p
                             className="text-black text-2xl font-bold">
                             登陆面试指导
                         </p>
                     </div>
                     <div
-                        className={ 'm-4 w-[300px] h-[190px] bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4' }>
-                        <Image src={ '/Group4598.svg' } alt={ '' } width={ 70 } height={ 70 }/>
+                        className={'py-5 m-5 bg-white rounded-lg shadow-lg flex flex-col items-center justify-center space-y-4'}>
+                        <Image src={'/Group4598.svg'} alt={''} width={70} height={70}/>
                         <p
                             className="text-black text-2xl font-bold">
                             安家生活服务
@@ -185,36 +179,35 @@ export default function MarketingPage(){
             </div>
         </div>
 
-        <div className={ 'w-full h-[855px] mt-20' }>
-            <div className={ 'relative w-full h-[557px]' }>
-                <div className={ 'absolute top-0 left-0 w-full h-full' }>
-                    <Province_banner banner={ '/pki7947412.png' }/>
+        <div className={'w-full h-[855px] mt-20'}>
+            <div className={'relative w-full h-[557px]'}>
+                <div className={'absolute top-0 left-0 w-full h-full'}>
+                    <Image src={'/pki7947412.png'} alt={'/'} layout='fill'/>
                 </div>
                 <div
-                    className={ 'w-full h-full absolute left-0 top-0 bg-gradient-to-r from-[#451411] to-[#5F100B5E] space-y-20' }>
-                    <div className={ 'text-center font-bold pt-10' }>
+                    className={'w-full h-full absolute left-0 top-0 bg-gradient-to-r from-[#451411] to-[#5F100B5E] space-y-20'}>
+                    <div className={'text-center font-bold pt-10'}>
                         <span className="text-[#E83328] text-[150px]">2000+</span>
-                        <p className={ 'text-6xl text-white font-light' }>申请人认可的移民服务品牌</p>
+                        <p className={'text-6xl text-white font-light'}>申请人认可的移民服务品牌</p>
                     </div>
                     <div
-                        className={ 'w-[1280px] h-[450px] bg-white mx-auto rounded-lg flex flex-col items-center justify-center space-x-6 p-6' }>
-                        <div className={ 'w-full flex space-x-4' }>
-                            <Image src={ '/161704431045_.pic.jpg' } alt={ '' } width={ 233 } height={300}
-                                 className={ 'shadow' }/>
-                            <Image src={ '/171704431045_.pic.jpg' } alt={ '' } width={ 233 } height={300}
-                                 className={ 'shadow' }/>
-                            <Image src={ '/181704431045_.pic.jpg' } alt={ '' } width={ 233 } height={300}
-                                 className={ 'shadow' }/>
-                            <Image src={ '/191704431045_.pic.jpg' } alt={ '' } width={ 233 } height={300}
-                                 className={ 'shadow' }/>
-                            <Image src={ '/201704431045_.pic.jpg' } alt={ '' } width={ 233 } height={300}
-                                 className={ 'shadow' }/>
+                        className={'w-[1280px] h-[450px] bg-white mx-auto rounded-lg flex flex-col items-center justify-center space-x-6 p-6'}>
+                        <div className={'w-full flex space-x-4'}>
+                            <Image src={'/161704431045_.pic.jpg'} alt={''} width={233} height={300}
+                                   className={'shadow'}/>
+                            <Image src={'/171704431045_.pic.jpg'} alt={''} width={233} height={300}
+                                   className={'shadow'}/>
+                            <Image src={'/181704431045_.pic.jpg'} alt={''} width={233} height={300}
+                                   className={'shadow'}/>
+                            <Image src={'/191704431045_.pic.jpg'} alt={''} width={233} height={300}
+                                   className={'shadow'}/>
+                            <Image src={'/201704431045_.pic.jpg'} alt={''} width={233} height={300}
+                                   className={'shadow'}/>
                         </div>
-                        <Link href={ '/all_profiles' }>
-                            <Button className={ 'm-6' } variant={ 'ghost' }>查看更多案例</Button>
+                        <Link href={'/all_profiles'}>
+                            <Button className={'m-6'} variant={'ghost'}>查看更多案例</Button>
                         </Link>
                     </div>
-
                 </div>
             </div>
         </div>
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,47 +1,55 @@
 import { Logo } from '@/app/(marketing)/_components/logo'
-import { Title } from '@/components/Title'
 import { LucideMailbox, MapPin, Phone } from 'lucide-react'
-import { DropdownMenuItemEach_employers } from '@/lib/publicResuces'
-import Link from 'next/link'
+import Image from "next/image";
 
-const Footer = ({ has_dialog }: { has_dialog?: boolean }) => {
-    return (<div className={ 'bg-mask-color5 px-20 text-white flex space-x-20' }>
-        <div className={ 'w-[500px] py-20' }>
+const Footer = () => {
+    return (<div className={ 'bg-mask-color5 grid grid-cols-3 text-white px-10 py-20' }>
+        <div className='grid-cols-1 px-5 space-y-4'>
             <Logo/>
-            <p className={ 'mt-10' }>
-                博泰移民（BoTai Immigration）
+            <p>
+                博泰移民 ( BoTa Visa )
                 <br/>
                 是一家致力于为个人和家庭提供专业移民服务的领先机构。
                 我们以多年的经验和深厚的专业知识，为客户提供全面的移民解决方案，助力他们实现全球范围内的移民梦想。
-                <br/>
-                选择博泰移民，意味着选择了专业、可信赖的移民合作伙伴。我们将竭诚为您提供卓越的移民服务，帮助您开启新的生活篇章。让博泰移民成为您实现全球梦想的助力，与您携手共创美好未来。
             </p>
         </div>
-        <div className={ 'w-[400px] py-20' }>
-            <Title size={ 30 } title={ '联系我们' }/>
-            <div className={ 'mt-16 space-y-6' }>
-                <div className={ 'flex space-x-3 ' }>
-                    <MapPin/>
-                    <p>1174 Pembina Hwy Winnipeg, MB R3T 2A4,</p>
+
+        <div className='grid-cols-1 space-y-6'>
+            <div className='font-bold text-4xl'>联系我们</div>
+
+            <div className='flex space-x-8'>
+                <div className={'text-center'}>
+                    <Image src={'/qrcoe1.webp'} alt={''} height={100} width={100}/>
+                    <span className={'pt-4 block'}>联系微信</span>
                 </div>
-                <div className={ 'flex space-x-3' }>
-                    <LucideMailbox/>
-                    <p>info@boatvisa.ca</p>
+                <div className={'text-center'}>
+                    <Image src={'/qrcoe1.webp'} alt={''} height={100} width={100}/>
+                    <span className={'pt-4 block'}>博泰公众号</span>
                 </div>
-                <div className={ 'flex space-x-3' }>
-                    <Phone/>
-                    <p>204-615-6555</p>
+                <div className={'text-center'}>
+                    <Image src={'/qrcoe1.webp'} alt={''} height={100} width={100}/>
+                    <span className={'pt-4 block'}>博泰小程序</span>
                 </div>
             </div>
+
+
         </div>
-        <div className={ 'w-[200px] py-20' }>
-            <Title size={ 30 } title={ '热门项目' }/>
-            <div className={ 'mt-16' }>
-                { DropdownMenuItemEach_employers.map((item, index) => {
-                    return (<Link key={ index } href={ item.href }>
-                        <p className={ 'pb-4 ease-in-out duration-300' }>{ item.text }</p>
-                    </Link>)
-                }) }
+
+        <div className='grid-cols-1 space-y-6'>
+            <div className='font-bold text-4xl'>公司地址</div>
+            <div className={'space-y-4'}>
+                <div className={'flex space-x-3 '}>
+                    <MapPin/>
+                    <span>1174 Pembina Hwy Winnipeg, MB R3T 2A4,</span>
+                </div>
+                <div className={'flex space-x-3'}>
+                    <LucideMailbox/>
+                    <span>info@boatvisa.ca</span>
+                </div>
+                <div className={'flex space-x-3'}>
+                    <Phone/>
+                    <span>204-615-6555</span>
+                </div>
             </div>
         </div>
     </div>)
