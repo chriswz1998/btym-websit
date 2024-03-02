00e2ff16557c45077a8177978216b7580461e718
pokemon <scissorwz@gmail.com>
2024-03-01 23:43:55 -0800

    fix build error

--- app/api/postArticles/route.ts
+++ app/api/postArticles/route.ts
@@ -1,25 +1,5 @@
 import { NextResponse } from 'next/server'
 import { db } from '@/lib/db'
-import {NextApiRequest, NextApiResponse} from "next";
-
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const id = req.query.id;
-        if (!id) {
-            res.status(400).json({ message: 'Bad Request: ID is required' });
-            return;
-        }
-        const postResult = await db.articles.findMany({
-            where: {
-                subSectionLinks_id: parseInt(id as string, 10)
-            }
-        });
-        res.status(200).json(postResult);
-    } catch (error) {
-        console.error('GET request error', error);
-        res.status(500).json({ message: 'Internal Error' });
-    }
-}
 
 export async function POST(req: Request) {
     try {
